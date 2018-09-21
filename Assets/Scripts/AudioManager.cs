23efd25379d50bab88d0039f2b3f3f4ebf0aaeea
SAE\rjansson <1008887@student.sae.edu.au>
2018-09-21 14:35:47 +1000

    FIXED A FADE IN FADE OUT ANIMATION whoops caps also added some more scripts

--- Assets/Scripts/AudioManager.cs
+++ Assets/Scripts/AudioManager.cs
@@ -4,13 +4,54 @@ using UnityEngine;
 
 public class AudioManager : MonoBehaviour {
 
-	// Use this for initialization
-	void Start () {
-		
+    private AudioSource source;
+
+    public AudioClip busMusic, memoryMusic, breathing, boom, heartbeat;
+
+    private void OnEnable()
+    {
+        EventManager.StartListening("OnMemoryGo", Memory);
+        EventManager.StartListening("OnSecondDown", EverySecond);
+    }
+
+    private void OnDisable()
+    {
+        EventManager.StopListening("OnMemoryGo", Memory);
+        EventManager.StopListening("OnSecondDown", EverySecond);
+    }
+
+    // Use this for initialization
+    void Start () {
+        source = GetComponent<AudioSource>();
 	}
-	
-	// Update is called once per frame
-	void Update () {
-		
-	}
+
+    public void Memory ()
+    {
+        StartCoroutine(FadeOut(source, 0.5f));
+        source.clip = memoryMusic;
+        source.Play();
+
+        source.PlayOneShot(breathing);
+        source.PlayOneShot(heartbeat);
+    }
+
+    public void EverySecond ()
+    {
+        source.PlayOneShot(boom);
+    }
+
+    public IEnumerator FadeOut(AudioSource audioSource, float FadeTime)
+    {
+        float startVolume = audioSource.volume;
+
+        while (audioSource.volume > 0)
+        {
+            audioSource.volume -= startVolume * Time.deltaTime / FadeTime;
+
+            yield return null;
+        }
+
+        audioSource.Stop();
+        audioSource.volume = startVolume;
+    }
 }
--- Assets/Scripts/MemorySwitch.cs
+++ Assets/Scripts/MemorySwitch.cs
@@ -4,13 +4,54 @@ using UnityEngine;
 
 public class MemorySwitch : MonoBehaviour {
 
-	// Use this for initialization
-	void Start () {
-		
-	}
-	
-	// Update is called once per frame
-	void Update () {
-		
-	}
+    public GameObject busScene;
+    public GameObject memoryScene;
+
+    public GameObject fadeImage;
+
+    public float waitTime;
+
+    private void OnEnable()
+    {
+        EventManager.StartListening("OnMemoryGo", SwitchToMemory);
+        EventManager.StartListening("OnMemoryStop", SwitchToBus);
+    }
+
+    private void OnDisable()
+    {
+        EventManager.StopListening("OnMemoryGo", SwitchToMemory);
+        EventManager.StopListening("OnMemoryStop", SwitchToBus);
+    }
+
+    public void SwitchToMemory ()
+    {
+        StartCoroutine(WaitForSwitch());
+    }
+
+    public void SwitchToBus ()
+    {
+        StartCoroutine(WaitForSwitch());
+    }
+
+    public IEnumerator WaitForSwitch()
+    {
+        var fadeAnim = fadeImage.GetComponent<Animator>();
+        fadeAnim.SetTrigger("FadeIn");
+
+        yield return new WaitForSeconds(waitTime);
+
+        if (busScene.activeInHierarchy)
+        {
+            
+            busScene.SetActive(false);
+            memoryScene.SetActive(true);
+        }
+
+        else if (memoryScene.activeInHierarchy)
+        {
+            busScene.SetActive(true);
+            memoryScene.SetActive(false);
+        }
+    }
 }
+
