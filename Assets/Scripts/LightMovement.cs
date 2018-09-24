a97c229d2354b44eef821afc68b6f89a8b1664dc
Unknown <1008887@student.sae.edu.au>
2018-09-24 22:54:14 +1000

    Streetlights
    added lights and scripts controlling them so it's like the bus is passing streetlights

---
+++ Assets/Scripts/LightMovement.cs
@@ -0,0 +1,22 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class LightMovement : MonoBehaviour {
+
+    public Rigidbody rb;
+    public float speed;
+
+    private void Update()
+    {
+        rb.velocity = Vector3.forward * speed;
+    }
+
+    private void OnTriggerEnter(Collider other)
+    {
+        if(other.gameObject.CompareTag("Destroy"))
+        {
+            Destroy(gameObject);
+        }
+    }
+}
---
+++ Assets/Scripts/StreetLights.cs
@@ -0,0 +1,26 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class StreetLights : MonoBehaviour {
+
+    public GameObject lightPrefab;
+    public Transform lightSpawn;
+
+    public float spawnRate;
+
+    private void OnEnable()
+    {
+        InvokeRepeating("SpawnLight", 0f, spawnRate);
+    }
+
+    private void OnDisable()
+    {
+        CancelInvoke();
+    }
+
+    public void SpawnLight ()
+    {
+        Instantiate(lightPrefab, lightSpawn);
+    }
+}
