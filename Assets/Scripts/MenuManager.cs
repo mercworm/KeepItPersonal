7988ec9935424cbc61efc158831d9309999065f3
Unknown <1008887@student.sae.edu.au>
2018-09-25 11:04:24 +1000

    Main Menu
    Fixed up the main menu so that's completely done now

--- Assets/Scripts/MenuManager.cs
+++ Assets/Scripts/MenuManager.cs
@@ -12,6 +12,8 @@ public class MenuManager : MonoBehaviour {
     //The two panels that hold the controls and credits information.
     public GameObject controlsScreen, creditsScreen;
 
+    public GameObject fadeScreen;
+
     //Uncomment this if you want to press a button on the keyboard to start the game.
 
     private void Update()
@@ -19,7 +21,8 @@ public class MenuManager : MonoBehaviour {
         if (Input.GetKeyDown(start))
         {
             Debug.Log("You have started the game!");
-            SceneManager.LoadScene("GameScene");
+            StartCoroutine(Fade());
+            
         }
 
         if (Input.GetKeyDown(quit))
@@ -31,6 +34,7 @@ public class MenuManager : MonoBehaviour {
         if (Input.GetKeyDown(controls))
         {
             Debug.Log("Toggling the controls!");
+            creditsScreen.SetActive(false);
             if (controlsScreen.activeInHierarchy) controlsScreen.SetActive(false);
             else controlsScreen.SetActive(true);
         }
@@ -38,9 +42,17 @@ public class MenuManager : MonoBehaviour {
         if (Input.GetKeyDown(credits))
         {
             Debug.Log("Toggling the credits!");
+            controlsScreen.SetActive(false);
             if (creditsScreen.activeInHierarchy) creditsScreen.SetActive(false);
             else creditsScreen.SetActive(true);
         }
+    }
+
+    public IEnumerator Fade()
+    {
+        fadeScreen.SetActive(true);
+        yield return new WaitForSeconds(1);
+        SceneManager.LoadScene("GameScene");
     }
 
 
